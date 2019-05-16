0b406b7a72c9d57420edc46222bee0380d8a0838
HarmanjotRajpal <50685520+HarmanjotRajpal@users.noreply.github.com>
2019-05-16 18:40:58 -0500

    Update doubly link list.py

--- doubly link list.py
+++ doubly link list.py
@@ -1,45 +1,151 @@
-#doubly link list (DLL)
+Skip to content
+ 
+Search or jump to…
 
-class DLL:
-	def __init__(self, val):
-		self.prev = None
-		self.next = None
-		self.val = val
-		
-def dll_append(node1, head):
-	curr = head
-	if head.prev is None and head.next is None:
-		curr.next = node1
-		node1.next = None
-		node1.prev = curr
-	else:
-		while curr is not None:
-			#print(node.val)
-			if curr.next is None:
-				#print("here")
-				curr.next = node1
-				node1.prev = curr
-				node1.next = None	
-			curr = curr.next
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@HarmanjotRajpal 
+0
+0 0 noopurkharche/dataStructuresAndAlgorithms
+ Code  Issues 0  Pull requests 0  Projects 0  Wiki  Insights
+dataStructuresAndAlgorithms/doublyLL.py
+@noopurkharche noopurkharche Update doublyLL.py
+f47f75c 2 hours ago
+133 lines (107 sloc)  3.53 KB
+    
 
-node = DLL(10)
-head = node
-#dll_append(node, head)
+# node class for doubly linked list
+class Node:
+    def __init__(self,data):
+       self.data = data
+       self.next = None
+       self.prev = None
+       
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+    
+    # insert the new node at the front of the list
+    def push(self, data):
+        new_node = Node(data)
+        new_node.prev = None
+        new_node.next = self.head
+        # check if head is not None 
+        if self.head is not None:
+            self.head.prev = new_node
+        self.head = new_node
+        return 
+    
+    # insert new node after the given node
+    def insertAfter(self, prev_node, data):
+        new_node = Node(data)
+        new_node.prev = prev_node
+        temp = prev_node.next 
+        new_node.next = temp
+        prev_node.next = new_node
+        if temp is not None:
+            temp.prev = new_node
+        return
+    
+    # append node at the end 
+    def append(self, data): 
+        new_node = Node(data)
+        
+        if self.head is None:
+            self.head = new_node
+            new_node.next = None
+            new_node.prev = None
+            return
+        
+        # Travers to the last node of the doubly LL
+        lastNode = self.head
+        while lastNode.next is not None:
+            lastNode = lastNode.next
+        
+        lastNode.next = new_node
+        new_node.prev = lastNode
+        new_node.next = None
+        return
+    
+    def printDll(self):
+        print("--------------------------------")
+        current = self.head
+        str1 = ""
+        while current is not None:
+            str1 = str1 + "<- " + str(current.data) + " -> "
+            current = current.next
+        print(str1)
+        
+    # insert node after node with given data 
+    def insertAfterData(self,data, new_data):
+        new_node = Node(new_data)
+        #print(new_node.data)
+        current = self.head
+        
+        while current is not None and current.data != data:
+            current = current.next
+        #print(current.data)
+        
+        if current is not None:
+            temp = current.next
+        
+        current.next = new_node
+        new_node.next = temp
+        
+        if temp is not None:
+            temp.prev = new_node
+        
+        new_node.next = temp
+        new_node.prev = current
+        
+        return    
+    
+    # remove the duplucates from the doubly linked list
+    def removeDuplicates(self):
+        
+        # dictionary to check for Duplicates
+        dict = {}
+        
+        current = self.head
+        
+        # loop until the end
+        while current is not None:
+            
+            # check if it is present in dictionary
+            if current.data not in dict:
+                dict[current.data] = 1
+            else:
+                print("Duplicate " + str(current.data))
+                
+                # store the previous node of the current node
+                prev_node = current.prev
+                
+                # check if the node is thee last node
+                if current.next is not None:
+                    prev_node.next = current.next
+                    current.next.prev = prev_node
+                else:
+                    # if duplicate node is the last Node
+                    prev_node.next = None
+                
+            current = current.next
 
-node1 = DLL(11)
-dll_append(node1, head)
+l1 = DoublyLinkedList()
+l1.append(1)
+l1.append(6)
+l1.append(2)
+l1.append(1)
+# at the start
+l1.push(7)
+# insert afetr the given node
+l1.insertAfter(l1.head, 8)
+l1.insertAfterData(6,10)
+l1.append(1)
+l1.append(1)
+print(l1.printDll())
+l1.removeDuplicates()
+print(l1.printDll())
 
-node2 = DLL(12)
-dll_append(node2, head)
-
-node3 = DLL(13)
-dll_append(node3, head)
-
-node4 = DLL(14)
-dll_append(node4, head)
-
-curr = head
-
-while curr:
-	print(' - ' + str(curr.val))
-	curr = curr.next
