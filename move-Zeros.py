0de1a97528fd810b2539f6dec93e304b38bdae21
HarmanjotRajpal <50685520+HarmanjotRajpal@users.noreply.github.com>
2019-05-20 22:17:14 -0500

    Update move-Zeros.py

--- move-Zeros.py
+++ move-Zeros.py
@@ -1,15 +1,13 @@
 #Input: [0,1,0,3,12]
 #Output: [1,3,12,0,0]
-res =[]
-        
-for i in nums:
-  if i !=0:
-    res.append(i)
-        
-for i in range(0, len(res)):
-  nums[i] = res[i]
-  
-for i in range(len(res), len(nums)):
-  nums[i] = 0
-        
-print(nums)
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """ 
+        count = 0
+        for i in range(0, len(nums)):
+            if (nums[i] != 0):
+                nums[count], nums[i] = nums[i], nums[count]
+                count = count + 1
+        return nums
