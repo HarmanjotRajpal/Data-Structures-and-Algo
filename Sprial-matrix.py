fb37b370b0cb6c7a245034bc42881f00b9ea89ad
HarmanjotRajpal <50685520+HarmanjotRajpal@users.noreply.github.com>
2019-06-16 17:34:11 -0500

    Update Sprial-matrix.py

--- Sprial-matrix.py
+++ Sprial-matrix.py
@@ -1,34 +1,42 @@
-
-matrix = [[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
-#len of row
-m = len(matrix)
-
-#len of column
-n = len(matrix[0])
-
-#starting index of row
-k=0 
-#starting index of column
-l=0 
-
-
-while k<m and l<n:
-	#row
-    for  i in range(l,n):
-        print(matrix [k][i])
-    k = k + 1
+# this method prints the give matrix in spiral form
+# @para[in] : 2D matrix
+# @para[out] : None
+def spiralPrint(matrix):
     
-	#columm
-    for i in range(k,m):
-        print(matrix[i][n-1])
-    n=n-1
+    # number of rows
+    m = len(matrix)
     
-	#last row
-    for i in range(n-1,l-1,-1):
-        print(matrix[m-1][i])
-    m = m-1
-
-	#reverse column
-
-        print(matrix[i][l])
-    l = l+1
+    # number of columns
+    n = len(matrix[0])
+    
+    # starting index for the row
+    k = 0
+    
+    # starting index for column
+    l = 0
+    
+    while k<m and l <n:
+        # print the first row #1,2,3,4
+        for i in range(l,n):
+            print(matrix[k][i])
+        k = k + 1
+        
+        # print the last column   #8,12,16
+        for i in range(k,m):
+            print(matrix[i][n-1])
+        n = n - 1
+        
+        # print the last row
+        if k < m :
+            for i in range(n-1,(l-1),-1):
+                print(matrix[m-1][i])
+            m = m - 1
+        
+        # print the first column in reversed
+        if l < n:
+            for i in range(m-1,k-1,-1):
+                print(matrix[i][l])
+            l = l + 1
+            
+matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+spiralPrint(matrix)
